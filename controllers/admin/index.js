05191d371450647bd837f3b3eaee16d25f3e4c47
Han <chelllagom@gmail.com>
2021-06-04 17:35:13 +0900

    feat: Shop Thumbnail build
    multer 1.4.2 라이브러리 추가, 이용하여 파일 업로드 기능 구현, Shop 등록 시 이미지 보이게 view 구현(detail.html)

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,12 +1,34 @@
 const { Router } = require('express');
 const router = Router();
 const ctrl = require('./admin.ctrl');
+//정적파일(이미지,css 등) 경로
+const path = require('path');
+//__dirname은 현재 폴더위치, 즉 controllers/admin까지 반환해줌
+//uploadDir은 root경로임
+const uploadDir = path.join(__dirname, '../../uploads');
+
+//multer 셋팅
+const multer  = require('multer');
+const storage = multer.diskStorage({
+    destination :  (req, file, callback) => { 
+        //이미지가 저장되는 도착지 지정
+        callback(null, uploadDir );
+    },
+    filename :  (req, file, callback) => { 
+        // shops-날짜.jpg(png) 저장, mimetype이 image/jpeg라서 split함
+        callback(null, 'shops-' + Date.now() + '.'+ file.mimetype.split('/')[1] );
+    }
+});
+
+const upload = multer({ storage: storage });
 
 router.get('/shops', ctrl.get_shops );
 
 router.get('/shops/write', ctrl.get_shops_write );
 
-router.post('/shops/write', ctrl.post_shops_write );
+//html의 input name이 thumbnail이기 때문에
+//파일 요청이 1건이면 single, 여러 건이면 array로 처리
+router.post('/shops/write', upload.single('thumbnail'), ctrl.post_shops_write );
 
 router.get('/shops/detail/:id', ctrl.get_shops_detail );
 
