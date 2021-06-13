78133fcdd8510adc2c96a987f74ea5943b6f25d3
Han <chelllagom@gmail.com>
2021-06-13 15:41:52 +0900

    feat: 페이스북 회원가입, 로그인 기능 추가
    Font Awesome을 사용하여 페이스북 아이콘 사용, AccessToken, RefreshToken 생성

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 const router = Router()
 
+router.get('/', async ( _ , res ) => {
+    res.render( 'admin/shops.html');
+});
 router.use('/admin', require('./admin'));
 router.use('/accounts', require('./accounts'));
+router.use('/auth', require('./auth'));
 
 module.exports = router;
